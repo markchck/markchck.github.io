eea62968c137ce85bbaa479bf2dde8418c81a60c
jemin_ki <markchck@gmail.com>
2025-09-23 23:55:53 +0900

    빌드

--- src/component/store/hook.ts
+++ src/component/store/hook.ts
@@ -7,7 +7,7 @@ import { KAKAO_SDK_JS_KEY, NAVER_MAP_CLIENT_ID } from "../../env"
 const baseUrl = import.meta.env.BASE_URL
 
 const NAVER_MAP_URL = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${NAVER_MAP_CLIENT_ID}`
-const KAKAO_SDK_URL = "https://developers.kakao.com/sdk/js/kakao.min.js" //!
+const KAKAO_SDK_URL = "https://developers.kakao.com/sdk/js/kakao.min.js"
 
 export const useNaver = () => {
   const { naver, setNaver } = useContext(StoreContext)
